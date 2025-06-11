14717bcf1b72ed3e12fe73a983768034b7ffc3c0
Gaming <bb466882@gmail.com>
2025-06-11 14:43:24 +0300

    updates

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ mongoose
   })
   .then(() => {
     console.log("✅ MongoDB connected to: Contact Us");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
-    );
+    app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
   })
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
@@ -53,7 +51,9 @@ const sendEmail = async ({ name, email, subject, message }) => {
 // Contact form endpoint
 app.post("/contact", async (req, res) => {
   const { formData } = req.body;
-
+  console.log("====================================");
+  console.log(formData);
+  console.log("====================================");
   if (
     !formData.name ||
     !formData.email ||
